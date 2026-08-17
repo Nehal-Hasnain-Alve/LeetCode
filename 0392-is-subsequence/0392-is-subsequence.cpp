418efba588d95071ad0d23e83f374518d0810446
NEHAL HASNAIN ALVE <nehalhasnain452@gmail.com>
2026-08-17 23:08:31 +0600

    Time: 0 ms (100%), Space: 8.6 MB (67.51%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+ public:
+  bool isSubsequence(string s, string t) {
+    int n = t.size();
+    int i = 0;
+    int j = 0;
+    while (i < n) {
+      if (t[i] == s[j]) {
+        j++;
+      }
+      i++;
+    }
+    if (j == s.size()) {
+      return true;
+    }
+    return false;
+  }
+};
